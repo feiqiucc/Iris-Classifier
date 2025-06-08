960fa92c21421a86a1c08847b1bcf11ac69c32da
Eric Yao <z9yao@uwaterloo.ca>
2025-06-07 22:53:44 -0400

    .

--- main.py
+++ main.py
@@ -2,25 +2,39 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+import pandas
 
-in_features = 16
-hidden_dim = 64
-out_features = 2
+df = pandas.read_csv('iris/iris.data')
+df = df.dropna()
+df = df.sample(120)
+
+X = torch.tensor(df.iloc[:, :4].values).float()
+Y = torch.tensor(df.iloc[:, 4].values)
+
+in_features = 4
+hidden_dim = 32
+out_features = 3
 
 model = nn.Sequential(
     nn.Linear(in_features, hidden_dim),
     nn.ReLU(),
     nn.Linear(hidden_dim, hidden_dim),
     nn.ReLU(),
-    nn.Linear(hidden_dim, out_features)
+    nn.Linear(hidden_dim, hidden_dim),
+    nn.ReLU(),
+    nn.Linear(hidden_dim, out_features),
+    nn.Sigmoid()
 )
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-for epoch in range(10):
-    outputs = model(...)
-    loss = criterion(outputs, ...)
+for epoch in range(1, 301):
+    outputs = model(X)
+    loss = criterion(outputs, Y)
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
+    if epoch % 30 == 0:
+        print(f'{epoch}\t: {loss.item():.3f}')
